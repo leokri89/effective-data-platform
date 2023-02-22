9d7a8126ae293989ab6e6431df8e2d7742bd7951
Leonardo <66442632+leokri89@users.noreply.github.com>
2023-02-22 15:32:27 -0300

    adicionado mkdocs no kubernetes usando nginx

---
+++ mkdocs-on-kubernets-nginx/build_image.cmd
@@ -0,0 +1,11 @@
+set IMG_NAME=dax-docs
+set IMG_VERSION=latest
+set CONTAINER_NAME=dax-docs-container
+
+echo "Criando imagem: %IMG_NAME%:%IMG_VERSION%"
+
+docker rm -f %CONTAINER_NAME%
+docker rmi -f %IMG_NAME%:%IMG_VERSION%
+docker build -t %IMG_NAME%:%IMG_VERSION% -f Dockerfile .
+
+docker run -d --rm -p 8080:8080 --name %CONTAINER_NAME% %IMG_NAME%:%IMG_VERSION%
